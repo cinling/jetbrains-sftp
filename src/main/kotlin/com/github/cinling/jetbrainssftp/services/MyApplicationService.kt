a0f74152f793640e3d028ab250fd9b092c4279b3
GitHub Action <action@github.com>
2022-03-04 13:35:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/cinling/jetbrainssftp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cinling/jetbrainssftp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cinling.jetbrainssftp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cinling.jetbrainssftp.MyBundle
 
 class MyApplicationService {
 
